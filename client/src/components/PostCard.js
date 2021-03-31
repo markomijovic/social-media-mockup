6d45334ec11efea56dbbad807f2b835d809ec8a7
Marko Mijovic <mijovicmarko11@gmail.com>
2021-03-31 01:03:07 -0600

    front end

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+function PostCard({
+    post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+
+    function likePost() {
+        console.log(likePost);
+    }
+
+    function commentOnPost() {
+        console.log(commentOnPost);
+    }
+    return (
+        <Card fluid>
+            <Card.Content>
+                <Image
+                    floated="right"
+                    size="mini"
+                    src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+                />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta as={Link} to={`/posts/${id}`}>
+                    {moment(createdAt).fromNow(true)}
+                </Card.Meta>
+                <Card.Description>{body}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button as='div' labelPosition='right' onClick={likePost}>
+                    <Button color = 'pink' basic>
+                        <Icon name='heart' />
+                    </Button>
+                    <Label basic color="pink" pointing="left">
+                        {likeCount}
+                    </Label>
+                </Button>
+                <Button as='div' labelPosition='right' onClick={commentOnPost}>
+                    <Button color = 'teal' basic>
+                        <Icon name='comment' />
+                    </Button>
+                    <Label basic color="teal" pointing="left">
+                        {commentCount}
+                    </Label>
+                </Button>
+            </Card.Content>
+        </Card>
+    );
+}
+
+export default PostCard;
